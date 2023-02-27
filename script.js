33bc3a661bcdab4e8f2992296f2e3da8de7c0572
mhmd-bal <mohammad.balouza@gmail.com>
2023-02-27 22:03:49 +0200

    Added list of words to guess during the game

---
+++ script.js
@@ -0,0 +1,25 @@
+let Words = {
+    Languages: [
+        "javascript",
+        "python",
+        "kotlin",
+        "swift",
+        "typescript",
+    ],
+
+    Food: [
+        "burger",
+        "pizza",
+        "taco",
+        "donuts",
+    ],
+
+    Countries: [
+        "america",
+        "lebanon",
+        "syria",
+        "canada",
+        "france",
+    ],
+}
+
