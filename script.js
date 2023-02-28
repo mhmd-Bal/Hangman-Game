4c8665c8e52e1559ebb81ca206360dbf6b5574e7
mhmd-bal <mohammad.balouza@gmail.com>
2023-02-28 09:24:39 +0200

    Added checkletterInWord function

--- script.js
+++ script.js
@@ -28,8 +28,9 @@ const printed_word_to_guess = document.getElementById("Word_to_guess");
 const keyboard_keys = document.getElementsByClassName("Key");
 let option = '';
 let word_to_guess = '';
-let guessed_letters = [];
+let guessed_letters = [" "];
 let game_started = false;
+let lives = 6;
 
 // Start game
 
@@ -39,7 +40,6 @@ for (let i=0; i < word_options.length; i++){
     word_options[i].addEventListener("click", () => {
         if(!game_started){
             option = word_options[i].textContent;
-            game_started = true;
             word_to_guess = generateRandomWord();
             console.log(word_to_guess);
             printGeneratedWord();
@@ -60,21 +60,51 @@ const generateRandomWord = () => {
     }
 
 }
+word_to_guess = word_to_guess.split("");
 
-const printGeneratedWord = () => {
-    word_to_guess = word_to_guess.split("");
-    for (i=0; i < word_to_guess.length; i++){
-        printed_word_to_guess.textContent += "_ ";
+const printGeneratedWord = (guessed_letters) => {
+    // for (i=0; i < word_to_guess.length; i++){
+    //     printed_word_to_guess.textContent += "_ ";
+    // }
+    printed_word_to_guess.textContent = " ";
+    if(game_started){
+        for (let i=0; i < word_to_guess.length; i++){
+            if(guessed_letters.indexOf(word_to_guess[i]) >= 0){
+                for(let j=0; j < guessed_letters.length; i++){
+                    if(word_to_guess[i] == guessed_letters[j]){
+                        printed_word_to_guess.textContent += letter_to_check;
+                    }
+                }
+            }else{
+                printed_word_to_guess.textContent += "_ ";
+            }
+    
+        }
+    }else{
+        for (let i=0; i < word_to_guess.length; i++){
+            printed_word_to_guess.textContent += "_ ";
+            game_started = true;
+        }
     }
+
 }
 
 for (let i=0; i < keyboard_keys.length; i++) {
     keyboard_keys[i].addEventListener("click", () => {
         if(game_started){
-            let key = '';
-            key = keyboard_keys[i].innerText;
-            checkKeyInWord(key)
+            let Letter = '';
+            Letter = keyboard_keys[i].innerText;
+            checkLetterInWord(Letter)
         }
     });
 }
 
+const checkLetterInWord = (Letter) => {
+    if (word_to_guess.indexOf(Letter) >= 0){
+        if(guessed_letters.indexOf(Letter) == -1){
+            guessed_letters.push(Letter);
+            console.log(guessed_letters);
+        // printGeneratedWord(guessed_letters);
+        }
+    }
+}
